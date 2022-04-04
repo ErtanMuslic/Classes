02e831248df8efb6bab6c0f648284473f9f68814
Ertan Muslic <ertanmuslic@gmail.com>
2022-04-04 18:12:33 +0200

    Javascript

---
+++ Javascript/Class-2/index.js
@@ -0,0 +1,50 @@
+var a= 10 / 2;
+var b=10 % 2;
+var a = 10>20;
+
+console.log(a,b);
+
+
+// var age = 20;
+// var fullGreetings= `I am ${age} years old`;
+// var greeetings = `I am ` + age + ` years old`;
+// console.log(fullGreetings);
+// console.log(greeetings);
+
+
+
+// var greeetings = "Hello coder";
+// var name = " Irfan";
+
+// var fullGreetings=greeetings + name;
+// console.log(fullGreetings);
+
+
+// var temp="Suad";
+// temp[0]=temp[0].toUpperCase; 
+// console.log(temp[0]);
+
+
+
+// var a =10;
+// var b=20;
+// var temp;
+
+// temp=a;
+// a=b;
+// b=temp;
+
+// console.log(a,b);
+
+
+
+
+// var broj = '10' - '20';
+// var broj = 10+'20';
+// broj=20;
+
+// broj='broj';
+// console.log((broj));
+// console.log(typeof broj);
+// console.log(typeof broj.toString());
+
