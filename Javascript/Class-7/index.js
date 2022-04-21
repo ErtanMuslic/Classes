7bb9e5ab3e86bf923c2b770a6e4fa3ff28857af0
Ertan Muslic <ertanmuslic@gmail.com>
2022-04-21 17:55:40 +0200

    Javascript

---
+++ Javascript/Class-7/index.js
@@ -0,0 +1,82 @@
+// var sum = 0;
+// var n = 20;
+// for(let i = 0; i < n ; i++){
+//     sum+=i;
+// }
+
+// console.log(sum);
+
+// function sumof(n) {
+//     var s=0;
+//     for(let i = 1;i<=n;i++){
+//         s+=i;
+//     }
+//     return s;
+// }
+
+// function sumR(n) {
+//     var sum = 0;
+//     if(n === 1){
+//         return 1;
+//     }
+
+//     sum = n + sumR(n-1); 
+//     return sum;
+// }
+
+// console.log(sumR(10));
+
+// function ProductR(n) {
+//     var sum = 1;
+//     if(n === 1){
+//         return 1;
+//     }
+
+//     sum = n * ProductR(n-1); 
+//     return sum;
+// }
+
+// console.log(ProductR(10));
+
+
+
+
+
+// function countdown(n){
+//     if(n === 1){
+//         return 1;
+//     }
+//     var string=`${n} ${countdown(n-1)}`;
+//     return string;
+// }
+
+
+
+// function countdown(n){
+//     if(n === 1){
+//         return [1];
+//     }
+//     var array=[n,...countdown(n-1)];
+
+//     return array;
+// }
+
+
+// console.log(countdown(4));
+
+
+
+
+// function range(a,b){
+//     var n = a;
+//     if(n === b){
+//         return b;
+//     }
+//     var string=`${n} ${range(n+1,b)}`;
+//     return string;
+// }
+
+// console.log(range(2,5));
+
+
+
