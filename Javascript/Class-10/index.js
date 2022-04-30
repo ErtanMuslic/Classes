59029917af9f287c84aee4d470a6bc1b62cf5005
Ertan Muslic <ertanmuslic@gmail.com>
2022-04-30 19:08:17 +0200

    Classes

---
+++ Javascript/Class-10/index.js
@@ -0,0 +1,38 @@
+// const obj = {user: 'suad' , age:29 , address: {street: 'hotkovo' , number: 0, city:"Novi Pazar"}
+// };
+
+// const {user,age, address} = obj;
+// const {street,number,city} = address;
+
+// console.log(street);
+
+// const array = [1,2,3,4,5];
+
+// const [x,,,y] = [1,2,3,4,5];
+
+// console.log(x,y);
+
+
+// class Car {
+//     constructor(type,price){
+//         this.type=type;
+//         this.price=price;
+//     }
+//     getPrice(){
+//         return this.price;
+//     }
+//     getType(){
+//         return this.type
+//     }
+//     ToString(){
+//        return JSON.stringify(this);
+//     }
+// }
+
+// const car1 = new Car("BMW",20000);
+
+// console.log(car1);
+
+// car1.ToString();
+// console.log(car1.ToString());
+
---
+++ Javascript/Class-9/index.js
@@ -0,0 +1,120 @@
+// for (let index = 0; index < 10; index++) {
+//     console.log(a);
+//     var a = 10;
+// }
+
+// const a = [20];
+// a.push(20);
+
+// console.log(a);
+
+// let b = 20;
+
+// function removeCharacter(title = "", character) {
+//    return title.replace(character,"");
+// }
+
+// console.log(removeCharacter("Irfan je c",'c'));
+
+
+// function removeCharacter(title = '', character) {
+//     return title.replace(character,"");
+//  }
+ 
+//  console.log(removeCharacter());
+
+
+
+//  () => {}
+
+//  const fn =() =>{}
+
+
+
+//  const removeCharacter2(title = "", character) => {
+//     return title.replace(character,"");
+//  }
+ 
+//  console.log(removeCharacter2());
+
+
+// let array1 = [1,2,34,5,6,7];
+// const array2 = array1;
+
+
+// array2 = []
+
+// console.log(array1,array2);
+
+
+
+
+// let o1 = {};
+// const o2 = o1;
+
+// o2.name="name";
+
+// console.log(o1,o2);
+
+
+
+// let array1 = [1,2,34,5,6,7];
+// const array2 = [...array1];
+
+
+// array2.push("s");
+
+// console.log(array1,array2);
+
+
+// let array1 = {name:"suad"};
+// const array2 = {...array1};
+
+
+// array2.name="irfan";
+
+// console.log(array1,array2);
+
+// function charsToString(...args) {
+//     let sum=''
+//     for (let index = 0; index < array.length; index++) {
+//         const element = array[index];
+//         sum+=element;
+//     }
+//     return sum;
+// }
+// conosle.log(charsToString(1,2,3,4,5,"23","23","2"));
+
+
+// const sum = (...args) => {
+//     let tempSum = 0;
+//     for (let index = 0; index < array.length; index++) {
+//         const element = array[index];
+//         tempSum+=element;      
+//     }
+//     return tempSum;
+// };
+
+// console.log(sum(1,2,3,4,5,6,"s"));
+
+
+const sum = (...args) => {
+    let tempSum = 0;
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+
+    if(typeof element === "number"){
+        tempSum +=element;
+
+    }
+
+    if(typeof element === "string"){
+        tempSum += parseInt(element);
+
+    }
+
+    return tempSum;
+}
+};
+
+console.log(sum(1,2,3,4,5,6,"s"));
