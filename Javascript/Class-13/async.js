6f8c8bdf308256b9f617b6f30d4c0efacd137358
Ertan Muslic <ertanmuslic@gmail.com>
2022-05-10 18:44:42 +0200

    Async

---
+++ Javascript/Class-13/async.js
@@ -0,0 +1,16 @@
+
+console.log("hi 1");
+
+const fn = () => {
+    console.log("hi");
+};
+// setTimeout(fn, 0);
+// setInterval(fn, 1000);  //Stopira se sa Ctrl+C
+
+const id = setInterval(fn, 1000);
+
+setTimeout(() => clearInterval(id), 5000); //Ispisuje "hi" 4 puta
+
+console.log("hi 2");
+
+
