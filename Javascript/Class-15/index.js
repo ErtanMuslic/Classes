4e9c719fd182b2c27f61a784bee57613484ce8bb
Ertan Muslic <ertanmuslic@gmail.com>
2022-05-14 18:29:36 +0200

    .

--- Javascript/Class-15/index.js
+++ Javascript/Class-15/index.js
@@ -1,9 +1,12 @@
 const fetch = require("node-fetch");
 const fn = async (author) =>{
-		const author = args.join("%20");
+		// const author = args.join("%20");
 		// const author = args[0]; 
 		const response = await fetch(`https://api.quotable.io/random${author ? `?author=${author}` : ""}`);//.then(response => {
     		const result = await response.json();
+
+			console.log(result);
+			console.log(result.content + "; author: " + result.author);
 };
 
 fn("Buddha");
