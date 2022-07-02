3ffbe03d0a76acf35b477bc62eeff02a09c695b7
Ertan Muslic <ertanmuslic@gmail.com>
2022-07-02 20:28:54 +0200

    fromdate

--- React/react2gen/src/components/class/fromdate.jsx
+++ React/react2gen/src/components/class/fromdate.jsx
@@ -3,16 +3,19 @@ class FromDate extends React.Component{
     constructor(props){
         super(props);
         const date = new Date(2022,6,1,5,23,59);
-        this.state={ dayYesterday:date,time:""}
+        this.state={ dayYesterday:date,time:this.calculateDateDiff()}
     }
 
     calculateDateDiff(){
-        
+    //    console.log(parseInt((new Date().getTime() / 1000).toFixed(0)));
+    // console.log(parseInt((new Date(2022,6,1,5,23,59).getTime() / 1000).toFixed(0)))
+     return  parseInt((new Date().getTime() / 1000).toFixed(0)) - parseInt((new Date(2022,6,1,5,23,59).getTime() / 1000).toFixed(0))
     }
-
     componentDidMount(){
         this.intervalId=setInterval(() =>{
-
+            this.setState({
+                time:this.state.time+1
+            });
         },1000)
     }
 
