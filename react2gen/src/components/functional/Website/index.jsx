18d9337270c0ae19e30c14e6afdf10eba411b841
Ertan Muslic <ertanmuslic@gmail.com>
2022-07-27 21:35:53 +0200

    Website

--- react2gen/src/components/functional/Website/index.jsx
+++ react2gen/src/components/functional/Website/index.jsx
@@ -6,10 +6,14 @@ import Nav from "./Nav";
 const Website = (props) => {
 
     return <div>
-        <Nav/>
-        <Hero/>
-        <Footer/>
-    </div>;
+    <Layout>
+      {url && url.length === 1 && <Hero />}
+      {url && url.includes("contacts") && <p>Contacts</p>}
+      {url && url.includes("price") && <p>Price</p>}
+      {url && url.includes("shop") && <p>Shop</p>}
+      {url && url.includes("about") && <p>about</p>}
+    </Layout>
+  </div>
 }
 
 export default Website;
