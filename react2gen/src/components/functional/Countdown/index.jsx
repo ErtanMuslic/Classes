b582f81ff71fcf7938586f03cb6322f0ea44e4b1
Ertan Muslic <ertanmuslic@gmail.com>
2022-07-29 17:38:43 +0200

    Library

--- react2gen/src/components/functional/Countdown/index.jsx
+++ react2gen/src/components/functional/Countdown/index.jsx
@@ -1,36 +1,38 @@
+import { intervalToDuration } from "date-fns";
 import { useState } from "react";
 import { useEffect } from "react";
 
 const Countdown = (props) => {
-    const {toDate,fromDate} = props;
-    const currentDate = new Date();
-    const [state,setState]= useState(0);
-    const [date,setDate] = useState(new Date());
+  const { toDate, fromDate } = props;
+  const [date, setDate] = useState(new Date());
 
-    const duration = intervalToDuration({
-        start: date,
-        end:new Date(2022,6,2,0,0,15),
-    })
+  const duration = intervalToDuration({
+    start: date,
+    end: new Date(2022, 7, 28, 18, 55, 0),
+  });
 
-    // useEffect(()=>{
-    //     setInterval(()=>{
-    //         useState(state + 1)
-    //     },1000);
-    // },[])
-    
-    useEffect(()=>{
-        setInterval(() =>{
-            setDate(new Date())
-        },1000)
-    },[])
+  //   useEffect(() => {
+  //     setInterval(() => {
+  //       setState(state + 1);
+  //     }, 1000);
+  //   }, []);
 
-    return <div style={{display:"flex"}}>
-        {duration.years > 0 && <p>{duration.years}Y, </p>}
-        {duration.months > 0 && <p>{duration.months}M, </p>}
-        {duration.days > 0 && <p>{duration.days}D, </p>}
-        {duration.hours > 0 && <p>{duration.hours}h, </p>}
-        {duration.minutes > 0 && <p>{duration.minutes}m, </p>}
-        {duration.seconds > 0 && <p>{duration.seconds}s</p>}
+  useEffect(() => {
+    setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+  }, []);
+
+  return (
+    <div style={{ display: "flex", gap: 10 }}>
+      {duration.years > 0 && <p> {duration.years}Y, </p>}
+      {duration.months > 0 && <p> {duration.months}M, </p>}
+      {duration.days > 0 && <p> {duration.days}d, </p>}
+      {duration.hours > 0 && <p> {duration.hours}h, </p>}
+      {duration.minutes > 0 && <p> {duration.minutes}m, </p>}
+      {duration.seconds > 0 && <p> {duration.seconds}s</p>}
     </div>
-}
+  );
+};
+
 export default Countdown;
