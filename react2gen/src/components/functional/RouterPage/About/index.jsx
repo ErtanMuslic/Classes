51c37fa9e11d08d4e4628e2460764f5de0142caf
Ertan Muslic <ertanmuslic@gmail.com>
2022-08-18 16:40:56 +0200

    Page

--- react2gen/src/components/functional/RouterPage/About/index.jsx
+++ react2gen/src/components/functional/RouterPage/About/index.jsx
@@ -1,14 +1,45 @@
 import { useNavigate } from "react-router-dom";
 import Layout from "../Layout";
+import style from "./About.module.css";
 
 const About =() =>{
-    const navigaton = useNavigate();
-    const redirectHome =() =>{
-        navigaton('/',{state:{name:"ertan"}})
-    }
+    // const navigaton = useNavigate();
+    // const redirectHome =() =>{
+    //     navigaton('/',{state:{name:"ertan"}})
+    // }
     return <Layout>
-        <p>About</p>
-        <button onClick={redirectHome}>Do something then go home</button>
+        <div className={style.About}>
+            <div style={{textAlign:"center"}}>
+                <img src="https://img-9gag-fun.9cache.com/photo/a3Q5VW5_460s.jpg"
+                 alt=""
+                 width="50px"
+                 height="50px" />
+                <p>Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+                <h4 style={{marginBottom:"0px",marginTop:"15%"}}>HOLDEN CAULFIELD</h4>
+                <p>Senior Product Designer</p>
+            </div>
+            <div style={{textAlign:"center"}}>
+            <img src="https://img-9gag-fun.9cache.com/photo/a3Q5VW5_460s.jpg"
+                 alt=""
+                 width="50px"
+                 height="50px" />
+                 <p>Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+                 <h4 style={{marginBottom:"0px",marginTop:"15%"}}>ALPER KAMU</h4>
+                <p>UI Develeoper</p>
+            </div>
+            <div style={{textAlign:"center"}}>
+            <img src="https://img-9gag-fun.9cache.com/photo/a3Q5VW5_460s.jpg"
+                 alt=""
+                 width="50px"
+                 height="50px"
+                 />
+                 <p>Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+                 <h4 style={{marginBottom:"0px",marginTop:"15%"}}>HENRY LETHAM</h4>
+                <p>CTO</p>
+            </div>
+            
+        </div>
+        {/* <button onClick={redirectHome}>Do something then go home</button> */}
     </Layout>
 }
 export default About;
--- react2gen/src/components/functional/RouterPage/Contact/index.jsx
+++ react2gen/src/components/functional/RouterPage/Contact/index.jsx
@@ -1,8 +1,21 @@
 import Layout from "../Layout";
+import style from "./Contact.module.css";
 
 const Contact =() =>{
     return <Layout>
-        <p>Contact</p>
+        <h1 style={{textAlign:"center",marginBottom:"0px"}}>Contact Us</h1>
+        <div className={style.ContactContainer}>
+            <div className={style.ContactTextArea}>
+                <textarea cols="30" rows="10">Message us....</textarea>
+            </div>
+            <div className={style.ContactInput}>
+                <label  htmlFor="">Name:</label>
+                <input className={style.ContactLabel} type="text" />
+                <label  htmlFor="">Email:</label>
+                <input  className={style.ContactLabel} type="email" />
+                <button className={style.Contactbutton}>Submit</button>
+            </div>
+        </div>
     </Layout>
 }
 export default Contact;
